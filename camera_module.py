aeab8544b2deb3a1c26518898dceae6c5ad14d49
Sophia Sun <sophiaphia@live.com>
2020-03-29 14:28:48 -0700

    test test

--- camera_module.py
+++ camera_module.py
@@ -12,8 +12,6 @@ bg = BrachioGraph(inner_arm=11, outer_arm=15, servo_1_degree_ms=10,
 				servo_2_degree_ms=10, servo_2_centre=1450, 
 				pw_down=1110, pw_up=1090, bounds=[-10.0, 9.0, 2.0, 20.0])
 
-bg.box()
-
 camera = PiCamera()
 camera.rotation = 180
 
