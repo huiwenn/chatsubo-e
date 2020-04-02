5949279e392b253c8adb3e46a7766806ae5e0fa9
Sophia Sun <sophiaphia@live.com>
2020-04-01 20:16:13 -0700

    ye

--- camera_module.py
+++ camera_module.py
@@ -9,10 +9,7 @@ parser.add_argument('contour', type=int, help='contour value. smaller is detaile
 parser.add_argument('hatch', type=int, help='hatch value. smaller is detailed and slower')
 
 args = parser.parse_args()
-
-bg = BrachioGraph(inner_arm=11, outer_arm=15, servo_1_degree_ms=10, 
-				servo_2_degree_ms=10, servo_2_centre=1450, 
-				pw_down=1110, pw_up=1090, bounds=[-10.0, 9.0, 2.0, 20.0])
+bg = BrachioGraph(inner_arm=11, outer_arm=15, servo_1_degree_ms=10, servo_2_degree_ms=10, servo_2_centre=1450, pw_down=1040, pw_up=1020, bounds=[-10.0, 9.0, 2.0, 23.0])
 
 camera = PiCamera()
 camera.rotation = 180
